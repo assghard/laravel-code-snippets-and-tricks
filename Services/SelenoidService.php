97c863f9508d0d85aaf08e60383815d0fed78498
assghard <ivan@actiss.pl>
2023-11-03 18:39:40 +0100

    selenoid service: updated

--- Services/SelenoidService.php
+++ Services/SelenoidService.php
@@ -4,8 +4,20 @@
  * Laravel service for Selenoid [A cross browser Selenium solution for Docker](https://aerokube.com)
  * Service allows a browser opening and window manipulation. It is using Docker to launch browsers.
  * App testing, website scraping, automation, ...
- * 
+ *
  * This service requires `php-webdriver/webdriver` package to be installed
+ *
+ *
+ * http://URL_OR_IP_ADDRESS:8080 - Selenoid GUI
+ *
+ * http://URL_OR_IP_ADDRESS:4444/wd/hub - Selenoid for PHP
+ * http://URL_OR_IP_ADDRESS:4444/status - Selenoid status and session list
+ *
+ *
+ * Setting up:
+ * Install Selenoid on your server: https://github.com/aerokube/selenoid
+ * Download CM (Configuration Manager): https://github.com/aerokube/cm/releases
+ * ./cm selenoid start --vnc --args "-limit 10", where "limit" is a session limit 
  */
 
 declare(strict_types=1);
@@ -13,14 +25,15 @@ declare(strict_types=1);
 namespace App\Services;
 
 use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
 
 class SelenoidService
 {
     public string $sessionId;
     protected string $selenoidUrl; // http://localhost:4444/wd/hub
     protected array $defaultCapabilities;
-    private RemoteWebDriver $driver;
-    private array $availableSelenoids;
+    protected RemoteWebDriver $driver;
+    protected array $availableSelenoids;
 
     public function __construct()
     {
@@ -29,11 +42,13 @@ class SelenoidService
         $this->setRandomSelenoid();
     }
 
+    public function __destruct()
+    {
+        $this->closeSession();
+    }
+
     /**
      * List of selenoids here
-     * 
-     * http://URL_OR_IP_ADDRESS:4444/wd/hub
-     * http://URL_OR_IP_ADDRESS:8080 - Selenoid GUI
      */
     public function getAvailableSelenoids(): array
     {
@@ -72,15 +87,9 @@ class SelenoidService
     }
 
     public function setCapabilities(array $capabilities = [
-        'version' => '81.0', // 101.0
         'browserName' => 'chrome',
         'enableVNC' => true,
         'timeZone' => 'Europe/Warsaw',
-        'sessionTimeout' => '1h',
-        '-session-delete-timeout' => '1h',
-        '-max-timeout' => '1h',
-        '-timeout' => '1h',
-        '-limit' => '1h',
     ]): void
     {
         $this->defaultCapabilities = $capabilities;
@@ -90,11 +99,6 @@ class SelenoidService
     {
         $this->driver = RemoteWebDriver::create($this->selenoidUrl, $this->defaultCapabilities, $connectionTimeoutMs, $requestTimeoutMs);
         $this->sessionId = $this->getSessionId();
-    }
-
-    public function connectToSession(string $sessionId, int $connectionTimeoutMs = 1 * 60 * 60 * 1000, int $requestTimeoutMs = 1 * 60 * 60 * 1000): void
-    {
-        $this->driver = RemoteWebDriver::createBySessionID($sessionId, $this->selenoidUrl, $connectionTimeoutMs, $requestTimeoutMs);
     }
 
     public function closeSession(): void
@@ -109,10 +113,47 @@ class SelenoidService
         return $this->driver->getSessionID();
     }
 
+    public function getDriver()
+    {
+        return $this->driver;
+    }
+
+    public function getRequest(string $url)
+    {
+        $this->driver->get($url);
+    }
+
     public function getPageSource(string $url)
     {
         $this->driver->get($url);
 
         return $this->driver->getPageSource();
+    }
+
+    public function clickElement(string $cssSelector)
+    {
+        $this->driver->findElement(WebDriverBy::cssSelector($cssSelector))->click();
+    }
+
+    public function executeJsScript(string $script): void
+    {
+        $this->driver->executeScript($script);
+    }
+
+    public function getDownloadedList(): array
+    {
+        $listUrl = str_replace('/wd/hub', '/download/'.$this->getSessionId(), $this->getSelenoidUrl());
+        $fileList = file_get_contents($listUrl);
+        if (empty($fileList)) {
+            return [];
+        }
+
+        $fileList = array_filter(explode("\n", strip_tags($fileList)));
+        foreach ($fileList as $k => $item) {
+            $fileList[$item] = $listUrl.'/'.rawurlencode($item);
+            unset($fileList[$k]);
+        }
+
+        return $fileList;
     }
 }
